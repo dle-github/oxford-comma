4228b189e9afa8471bf70c67f87081ea6b36e8a6
derek <derekle.creative@gmail.com>
2021-01-16 22:26:43 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array.join()
+    elsif 
+        array.length == 2
+        return array.join (" and ")
+    else
+        array.join(", ")    
+        lastinList = "and #{array.last}"
+        array.pop
+        array << lastinList
+        return array.join(", ")
+    end
 end
+
+
+
